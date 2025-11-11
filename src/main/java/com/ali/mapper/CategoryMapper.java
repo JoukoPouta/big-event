8bfa14e68bfe54781551c702eb020ebed72da7ab
JoukoPouta <1977799823@qq.com>
2025-11-11 10:33:33 +0800

    文章列表更新

---
+++ src/main/java/com/ali/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.ali.mapper;
+
+import com.ali.pojo.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryMapper {
+    //添加
+    @Insert("insert into category(category_name,category_alias,create_user,create_time,update_time)" +
+            "values (#{categoryName},#{categoryAlias},#{createUser},#{createTime},#{updateTime})"
+    )
+    void add(Category category);
+}
---
+++ src/main/java/com/ali/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.ali.service;
+
+import com.ali.pojo.Category;
+
+public interface CategoryService {
+    void add(Category category);
+}
