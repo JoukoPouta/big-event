2a244995252fd2b4e38658b964684692d75e312d
JoukoPouta <1977799823@qq.com>
2025-11-13 15:00:34 +0800

    功能完善

--- src/main/java/com/ali/controller/ArticleController.java
+++ src/main/java/com/ali/controller/ArticleController.java
@@ -1,13 +1,12 @@
 package com.ali.controller;
 
 import com.ali.pojo.Article;
+import com.ali.pojo.PageBean;
 import com.ali.pojo.Result;
 import com.ali.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/article")
@@ -17,6 +16,31 @@ public class ArticleController {
     @PostMapping
     public Result add(@RequestBody @Validated Article article){
         articleService.add(article);
+        return Result.success();
+    }
+    @GetMapping
+    public Result<PageBean<Article>> list(
+            Integer pageNum,
+            Integer pageSize,
+            @RequestParam(required = false) Integer categoryId,
+            @RequestParam(required = false) String state
+    ){
+        PageBean<Article> pb = articleService.list(pageNum,pageSize,categoryId,state);
+        return Result.success(pb);
+    }
+    @GetMapping("/detail")
+    public Result<Article> detail(Integer id){
+        Article article = articleService.findById(id);
+        return Result.success(article);
+    }
+    @DeleteMapping
+    public Result delete(@Validated Integer id){
+        articleService.deleteById(id);
+        return Result.success();
+    }
+    @PutMapping
+    public Result update(@RequestBody @Validated Article article){
+        articleService.update(article);
         return Result.success();
     }
 }
--- src/main/java/com/ali/service/ArticleService.java
+++ src/main/java/com/ali/service/ArticleService.java
@@ -1,7 +1,17 @@
 package com.ali.service;
 
 import com.ali.pojo.Article;
+import com.ali.pojo.PageBean;
 
 public interface ArticleService {
+    //添加
     void add(Article article);
+    //条件分页列表查询
+    PageBean<Article> list(Integer pageNum, Integer pageSize, Integer categoryId, String state);
+
+    Article findById(Integer id);
+
+    void deleteById(Integer id);
+
+    void update(Article article);
 }
